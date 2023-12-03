db6310a3da54be10b5b14246eb4bdd7d0877a926
Sonuprasanth <106568097+Sonuprasanth@users.noreply.github.com>
2023-12-03 11:06:54 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,9 @@
+/**
+ *  In main file
+ *  let script1 = require('./script1');
+ *  console.log(script1.sum(1, 2));
+ */
+for(count=1;count<=100;count=count+1)
+{
+  console.log("satvik sai is a good boy and adhira is a cute girl");
+}
---
+++ script2.js
@@ -0,0 +1,19 @@
+/**
+ *  In main file
+ *  let script2 = require('./script2');
+ *  console.log(script2.sum(1, 2));
+ */
+
+var(traffic light="red");
+if(light="red");
+{
+  console.log("stop");
+}
+else if(light="green");
+{
+  console.log("get ready");
+}
+else if(light="yellow ");
+{
+  console.log("go");
+}
